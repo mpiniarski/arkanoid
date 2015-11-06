b0613d556fdb415ec1867164af51ee291080f945
Joanna Sołomiewicz <joanna.solomiewicz@student.put.poznan.pl>
2015-11-06 23:20:40 +0100

    Collision with platform
    Added collision with corners and sides.

--- Ball.cpp
+++ Ball.cpp
@@ -5,16 +5,16 @@
 using namespace std;
 
 Ball::Ball(Scene *scene, const sf::Texture &texture) : GraphicalEntity( scene, texture) {
-    movingHorizontal = -100;
-    movingVertical = 100;
-    velocity = 3.0;
+    movingHorizontal = 50;
+    movingVertical = 50;
+    velocity = 6.0;
     this->platform = NULL;
 }
 
 Ball::Ball(Scene *scene, const sf::Texture &texture, Platform* platform) : GraphicalEntity( scene, texture) {
-    movingHorizontal = -100;
-    movingVertical = 100;
-    velocity = 3.0;
+    movingHorizontal = 50;
+    movingVertical = 50;
+    velocity = 6.0;
     this->platform = platform;
 }
 
@@ -29,22 +29,53 @@ void Ball::wallCollision() {
 
 void Ball::detectCollision(GraphicalEntity *ge) {
 
-    float ballCenter = this->getPosition().x + this->getWidth()/2;
+    float ballCenterWidth = this->getPosition().x + this->getWidth()/2;
+    float ballCenterHeight = this->getPosition().y + this->getHeight()/2;
+    float radius = this->getWidth()/2;
     float platformCenter = ge->getPosition().x + ge->getWidth()/2;
     float platformEnd = ge->getPosition().x + ge->getWidth();
     float platformStart = ge->getPosition().x;
 
-    if(this->getPosition().y + this->getHeight() > ge->getPosition().y) {
-        if(ballCenter >= platformStart && ballCenter < platformCenter) {
-            movingHorizontal = (-1) * ((ballCenter - platformCenter) * 100)/(platformStart - platformCenter);
-            movingVertical = (-1) * sqrt((20000-(movingHorizontal*movingHorizontal)));
-//            movingVertical = (-1) * ((ballCenter - platformStart) * 100)/(platformCenter - platformStart);
+    /** AREA 1 **/
+    if((ballCenterWidth + radius/sqrt(2)) > platformStart
+       && ballCenterWidth - radius/sqrt(2) < platformStart
+       && ballCenterHeight + radius/sqrt(2) > ge->getPosition().y
+       && ballCenterHeight - radius/sqrt(2) < ge->getPosition().y) {
+        movingHorizontal *= -1;
+        movingVertical *= -1;
+    }
+    /** AREA 2 **/
+    if(ballCenterHeight + radius > ge->getPosition().y) {
+        if(ballCenterWidth >= platformStart && ballCenterWidth < platformCenter) {
+            movingHorizontal = (-1) * ((ballCenterWidth - platformCenter) * 100)/(platformStart - platformCenter);
+            movingVertical = (-1) * sqrt((10000-(movingHorizontal*movingHorizontal)));
+//            movingVertical = (-1) * ((ballCenterWidth - platformStart) * 100)/(platformCenter - platformStart);
         }
-        else if(ballCenter >= platformCenter && ballCenter <= platformEnd) {
-            movingHorizontal = ((ballCenter - platformCenter) * 100)/(platformEnd - platformCenter);
-            movingVertical = (-1) * sqrt((20000-(movingHorizontal*movingHorizontal)));
-//            movingVertical = (-1) * ((ballCenter - platformEnd) * 100)/(platformCenter - platformEnd);
+        else if(ballCenterWidth >= platformCenter && ballCenterWidth <= platformEnd) {
+            movingHorizontal = ((ballCenterWidth - platformCenter) * 100)/(platformEnd - platformCenter);
+            movingVertical = (-1) * sqrt((10000-(movingHorizontal*movingHorizontal)));
+//            movingVertical = (-1) * ((ballCenterWidth - platformEnd) * 100)/(platformCenter - platformEnd);
         }
+    }
+    /** AREA 3 **/
+    if((ballCenterWidth - radius/sqrt(2)) < platformEnd
+            && ballCenterWidth + radius/sqrt(2)> platformEnd
+            && ballCenterHeight + radius/sqrt(2) > ge->getPosition().y
+            && ballCenterHeight - radius/sqrt(2)< ge->getPosition().y) {
+        movingHorizontal *= -1;
+        movingVertical *= -1;
+    }
+    /** AREA 4 **/
+    if(ballCenterWidth + radius > platformStart
+       && ballCenterWidth - radius < platformStart
+       && ballCenterHeight > ge->getPosition().y) {
+        movingHorizontal *= -1;
+    }
+    /** AREA 5 **/
+    if(ballCenterWidth - radius < platformEnd
+       && ballCenterWidth + radius > platformEnd
+       && ballCenterHeight > ge->getPosition().y) {
+        movingHorizontal *= -1;
     }
 }
 
